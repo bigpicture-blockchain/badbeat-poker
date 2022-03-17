001f3e359278ea55a5f1032814f9c0a1f088e0e8
himanshupant <himanshu.pant@badbeat.com>
2022-03-17 17:56:27 +0530

    poker client and engine and doc url fix

---
+++ poker-engine/src/model/table/DefaultTableConfig.ts
@@ -0,0 +1,124 @@
+import { TableConfig } from "../TableConfig"
+
+const configs: TableConfig[] = [
+    {
+        "_id": undefined,
+        "name": "Palermo",
+        "smallBlindUsd": 5,
+        "bigBlindUsd": 10,
+        "currency": "usd",
+        "maxPlayers": 9,
+        "timeToActSec": 60,
+        "orderIndex": 1,
+        "maxBuyIn": 100,
+        "rake": 0.1
+    },
+    {
+        "_id": undefined,
+        "name": "Dublin",
+        "smallBlindUsd": 5,
+        "bigBlindUsd": 10,
+        "currency": "usd",
+        "maxPlayers": 9,
+        "timeToActSec": 60,
+        "orderIndex": 4,
+        "maxBuyIn": 100,
+        "rake": 1
+    },
+    {
+        "_id": undefined,
+        "name": "Belgrade",
+        "smallBlindUsd": 5,
+        "bigBlindUsd": 10,
+        "currency": "usd",
+        "maxPlayers": 9,
+        "timeToActSec": 60,
+        "orderIndex": 11,
+        "maxBuyIn": 100,
+        "rake": 1
+    },
+    {
+        "_id": undefined,
+        "name": "Hyderabad",
+        "smallBlindUsd": 5,
+        "bigBlindUsd": 10,
+        "currency": "usd",
+        "maxPlayers": 9,
+        "timeToActSec": 60,
+        "orderIndex": 11,
+        "maxBuyIn": 100,
+        "rake": 1
+    }, 
+    {
+        "_id": undefined,
+        "name": "Paris",
+        "smallBlindUsd": 5,
+        "bigBlindUsd": 10,
+        "currency": "usd",
+        "maxPlayers": 9,
+        "timeToActSec": 60,
+        "orderIndex": 11,
+        "maxBuyIn": 100,
+        "rake": 1
+    },    {
+        "_id": undefined,
+        "name": "Madrid",
+        "smallBlindUsd": 5,
+        "bigBlindUsd": 10,
+        "currency": "usd",
+        "maxPlayers": 9,
+        "timeToActSec": 60,
+        "orderIndex": 1,
+        "maxBuyIn": 100,
+        "rake": 0.1
+    },
+    {
+        "_id": undefined,
+        "name": "Faro",
+        "smallBlindUsd": 5,
+        "bigBlindUsd": 10,
+        "currency": "usd",
+        "maxPlayers": 9,
+        "timeToActSec": 60,
+        "orderIndex": 4,
+        "maxBuyIn": 100,
+        "rake": 1
+    },
+    {
+        "_id": undefined,
+        "name": "New York",
+        "smallBlindUsd": 5,
+        "bigBlindUsd": 10,
+        "currency": "usd",
+        "maxPlayers": 9,
+        "timeToActSec": 60,
+        "orderIndex": 11,
+        "maxBuyIn": 100,
+        "rake": 1
+    },
+    {
+        "_id": undefined,
+        "name": "Las Vegas",
+        "smallBlindUsd": 5,
+        "bigBlindUsd": 10,
+        "currency": "usd",
+        "maxPlayers": 9,
+        "timeToActSec": 60,
+        "orderIndex": 11,
+        "maxBuyIn": 100,
+        "rake": 1
+    }, {
+        "_id": undefined,
+        "name": "Miami",
+        "smallBlindUsd": 5,
+        "bigBlindUsd": 10,
+        "currency": "usd",
+        "maxPlayers": 9,
+        "timeToActSec": 60,
+        "orderIndex": 11,
+        "maxBuyIn": 100,
+        "rake": 1
+    }
+]
+
+export default configs;
---
+++ poker-engine/src/model/table/RewardsDetails.ts
@@ -0,0 +1,12 @@
+// import { GameResultPlayer } from "./GameResultPlayer";
+// import { DbPotResult } from "./DbPotResult";
+// import { TableAuditEvent } from "./TableAuditEvent";
+export class RewardsDetails {
+    date: Date;
+    guid: string;
+    profitLoss: number;
+    handRank: number;
+    handRankEnglish: string;
+    lastStreet: string;
+    winHand: boolean;
+}
